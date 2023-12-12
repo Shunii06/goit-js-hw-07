8708fe46d92c51bc623787340833e8c9f8d5b5e2
Piotrek <138370309+Shunii06@users.noreply.github.com>
2023-12-12 21:35:47 +0100

    02-lightbox added and working

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const ul = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryItems.forEach(({ preview, original, description }) => {
+  const li = document.createElement("li");
+
+  const galleryItem = document.createElement("a");
+  galleryItem.classList.add("gallery__item");
+  galleryItem.href = original;
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = preview;
+  img.alt = description;
+
+  galleryItem.appendChild(img);
+  li.appendChild(galleryItem);
+  ul.appendChild(li);
+});
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
